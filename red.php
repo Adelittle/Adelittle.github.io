0a9d06b122e2b09719a7849d6e11657b88e27d34
Adelittle <58516494+Adelittle@users.noreply.github.com>
2021-11-12 02:42:17 +0700

    Create red.php

---
+++ red.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>Instagram</title>
+   </head>
+   <body>
+      <a href="/loc.php?url=https://instagram.com:1234/"></a>
+      <script>
+         document.querySelector("a").click();
+      </script>
+      <div id="1"><font size="15">FBInbox : Rahul Kankrale</font></div>
+   </body>
+</html>
